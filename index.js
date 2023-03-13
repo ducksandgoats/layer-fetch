be5c96c15c8d1c7734d82a731d9b1244ee63c79a
ducksandgoats <mrtraderjoseph@gmail.com>
2023-03-12 21:28:55 -0500

    update the package

--- index.js
+++ index.js
@@ -81,7 +81,9 @@ module.exports = async function makeOnionFetch (opts = {}) {
     delete request.url
     const mainTimeout = reqHeaders.has('x-timer') || mainURL.searchParams.has('x-timer') ? reqHeaders.get('x-timer') !== '0' || mainURL.searchParams.get('x-timer') !== '0' ? Number(reqHeaders.get('x-timer') || mainURL.searchParams.get('x-timer')) * 1000 : undefined : useTimeOut
     
-    return sendTheData(signal, await handleData(mainTimeout, nodeFetch(useLink, request)))
+    const mainData = await handleData(mainTimeout, nodeFetch(useLink, request))
+
+    return sendTheData(signal, mainData)
   }
   
   router.any('lok://*/**', handleLok)
